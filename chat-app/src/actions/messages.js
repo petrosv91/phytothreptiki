c407dfbf8d9c4b659f1ad89dc4155e60ed77b3bc
petrosv890 <petrosv890@gmail.com>
2020-08-03 23:36:40 +0300

    Purge

--- chat-app/src/actions/messages.js
+++
@@ -1,8 +0,0 @@
-import { NEW_MESSAGE } from './types';
-import uuid from 'uuid/v4'; // returns unique ip string that is partly based on the current timestamp
-// allong with some randomly generated characters
-
-export const newMessage = ({ text, username }) => ({
-  type: NEW_MESSAGE,
-  item: { id: uuid(), timestamp: Date.now(), text, username }
-});
--- trivia-app/src/components/shared/Layout.js
+++
@@ -1,6 +0,0 @@
-/** @format */
-
-import styled from './styled-components/macro';
-import device from '../../config/device';
-
-export const Layout = styled.div``;
