e13285a26c27b63bd470462986f5317482156f92
vpe91 <vpe@connect-line.gr>
2020-09-18 18:12:18 +0300

    add elements

--- phytothreptiki_production_app/src/components/recipe/recipe.js
+++ phytothreptiki_production_app/src/components/recipe/recipe.js
@@ -1,7 +1,8 @@
 import { Flex } from '@chakra-ui/core';
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { FormInput, Buttons } from '../../lib/ui/';
+import { elements } from '../../config/elements';
+import { FormInput, Buttons, FormTagBox } from '../../lib/ui/';
 
 export default function Recipe() {
   const { register, handleSubmit, erros } = useForm();
@@ -9,19 +10,20 @@ export default function Recipe() {
   function onSubmit() {}
 
   return (
-    <Flex w='full'>
-      <form w='full' onSubmit={handleSubmit(onSubmit)}>
-        <Flex justify='space-between'>
-          <FormInput name='date' label='Ημερομηνία' formRef={register} />
-          <FormInput name='type' label='Τύπος' formRef={register} />
-          <FormInput name='recipe' label='Συνταγή' formRef={register} />
-        </Flex>
-        <Flex w='full' justify='flex-end'>
-          <Buttons.Primary ml='auto' type='submit'>
-            Καταχώρηση
-          </Buttons.Primary>
-        </Flex>
-      </form>
-    </Flex>
+    <form style={{ width: '100%' }} onSubmit={handleSubmit(onSubmit)}>
+      <Flex w='full' justify='space-evenly' align='center'>
+        <FormInput name='date' label='Ημερομηνία' formRef={register} />
+        <FormInput name='type' label='Τύπος' formRef={register} />
+        <FormInput name='recipe' label='Συνταγή' formRef={register} />
+      </Flex>
+      {/* {elements.map(el => <Flex>
+        <FormTagBox name=''/>
+      </Flex>)} */}
+      <Flex w='full' justify='flex-end'>
+        <Buttons.Primary ml='auto' type='submit'>
+          Καταχώρηση
+        </Buttons.Primary>
+      </Flex>
+    </form>
   );
 }
---
+++ phytothreptiki_production_app/src/config/elements.js
@@ -0,0 +1,122 @@
+export const elements = [
+  {
+    id: 1,
+    name: 'Μ.Α.Π',
+    formula: [12, 61, 0],
+  },
+  {
+    id: 2,
+    name: 'ΝΙΤΡΙΚΟ ΚΑΛΙΟ',
+    formula: [13, 0, 46],
+  },
+  {
+    id: 3,
+    name: 'ΦΩΣΦΟΡΙΚΟ ΚΑΛΙΟ',
+    formula: [],
+  },
+  {
+    id: 4,
+    name: 'ΟΥΡΙΑ',
+    formula: [46, 0, 0],
+  },
+  {
+    id: 5,
+    name: 'ΦΩΣΦΟΡΙΚΗ ΟΥΡΙΑ',
+    formula: [],
+  },
+  {
+    id: 6,
+    name: 'ΘΕΙΚΟ ΚΑΛΙΟ',
+    formula: [],
+  },
+  {
+    id: 7,
+    name: 'ΘΕΙΚΗ ΑΜΜΩΝΙΑ',
+    formula: [21, 0, 0],
+  },
+  {
+    id: 8,
+    name: 'ΘΕΙΚΟ ΜΑΓΝΗΣΙΟ',
+    formula: [],
+  },
+  {
+    id: 9,
+    name: 'ΟΥΡΙΑ ΧΑΜΗΛΗΣ ΔΙΟΥΡΙΑΣ',
+    formula: [],
+  },
+  {
+    id: 10,
+    name: 'ΝΙΤΡΙΚΟ ΑΣΒΕΣΤΙΟ',
+    formula: [],
+  },
+  {
+    id: 11,
+    name: 'ΝΙΤΡΙΚΟ ΜΑΓΝΗΣΙΟ',
+    formula: [],
+  },
+  {
+    id: 12,
+    name: 'ΙΧΝΟΣΤΟΙΧΕΙΑ',
+    formula: [],
+  },
+  {
+    id: 13,
+    name: 'ALGIFLOR',
+    formula: [],
+  },
+  {
+    id: 14,
+    name: 'BETAFIN',
+    formula: [],
+  },
+  {
+    id: 15,
+    name: 'AMINOL',
+    formula: [],
+  },
+  {
+    id: 16,
+    name: 'ΣΙΔΗΡΟΣ FERRIPLUS',
+    formula: [],
+  },
+  {
+    id: 17,
+    name: 'ΒΟΡΑΚΑΣ SOLUBOR',
+    formula: [],
+  },
+  {
+    id: 18,
+    name: 'ΣΙΔΗΡΟΣ Fe',
+    formula: [],
+  },
+  {
+    id: 19,
+    name: 'ΨΕΥΔΑΡΓΗΡΟΣ Zn',
+    formula: [],
+  },
+  {
+    id: 20,
+    name: 'ΜΑΓΓΑΝΙΟ Mn',
+    formula: [],
+  },
+  {
+    id: 21,
+    name: 'ΧΑΛΚΟΣ Cu',
+    formula: [],
+  },
+  {
+    id: 22,
+    name: 'MgO EDTA',
+    formula: [],
+  },
+  {
+    id: 23,
+    name: 'Ca EDTA',
+    formula: [],
+  },
+  {
+    id: 24,
+    name: 'ΜΟΛΥΒΔΑΙΝΙΟ Mo',
+    formula: [],
+  },
+];
