5dcefc4699472ff5994acacafc366ed6fe4263e0
petrosv890 <petrosv890@gmail.com>
2020-09-21 19:24:34 +0300

    update elements name & label

--- phytothreptiki_production_app/src/config/elements.js
+++ phytothreptiki_production_app/src/config/elements.js
@@ -1,122 +1,146 @@
 export const elements = [
   {
     id: 1,
-    name: 'Μ.Α.Π',
-    formula: [12, 61, 0],
+    name: 'map',
+    label: 'Μ.Α.Π',
+    formula: [12, 61, 0]
   },
   {
     id: 2,
-    name: 'ΝΙΤΡΙΚΟ ΚΑΛΙΟ',
-    formula: [13, 0, 46],
+    name: 'kno3',
+    label: 'ΝΙΤΡΙΚΟ ΚΑΛΙΟ',
+    formula: [13, 0, 46]
   },
   {
     id: 3,
-    name: 'ΦΩΣΦΟΡΙΚΟ ΚΑΛΙΟ',
-    formula: [],
+    name: 'mkp',
+    label: 'ΦΩΣΦΟΡΙΚΟ ΚΑΛΙΟ',
+    formula: []
   },
   {
     id: 4,
-    name: 'ΟΥΡΙΑ',
-    formula: [46, 0, 0],
+    name: 'uric',
+    label: 'ΟΥΡΙΑ',
+    formula: [46, 0, 0]
   },
   {
     id: 5,
-    name: 'ΦΩΣΦΟΡΙΚΗ ΟΥΡΙΑ',
-    formula: [],
+    name: 'up',
+    label: 'ΦΩΣΦΟΡΙΚΗ ΟΥΡΙΑ',
+    formula: []
   },
   {
     id: 6,
-    name: 'ΘΕΙΚΟ ΚΑΛΙΟ',
-    formula: [],
+    name: 'sop',
+    label: 'ΘΕΙΚΟ ΚΑΛΙΟ',
+    formula: []
   },
   {
     id: 7,
-    name: 'ΘΕΙΚΗ ΑΜΜΩΝΙΑ',
-    formula: [21, 0, 0],
+    name: 'so3',
+    label: 'ΘΕΙΚΗ ΑΜΜΩΝΙΑ',
+    formula: [21, 0, 0]
   },
   {
     id: 8,
-    name: 'ΘΕΙΚΟ ΜΑΓΝΗΣΙΟ',
-    formula: [],
+    name: 'mgs04',
+    label: 'ΘΕΙΚΟ ΜΑΓΝΗΣΙΟ',
+    formula: []
   },
   {
     id: 9,
-    name: 'ΟΥΡΙΑ ΧΑΜΗΛΗΣ ΔΙΟΥΡΙΑΣ',
-    formula: [],
+    name: 'urialB',
+    label: 'ΟΥΡΙΑ ΧΑΜΗΛΗΣ ΔΙΟΥΡΙΑΣ',
+    formula: []
   },
   {
     id: 10,
-    name: 'ΝΙΤΡΙΚΟ ΑΣΒΕΣΤΙΟ',
-    formula: [],
+    name: 'calciumNitrate',
+    label: 'ΝΙΤΡΙΚΟ ΑΣΒΕΣΤΙΟ',
+    formula: []
   },
   {
     id: 11,
-    name: 'ΝΙΤΡΙΚΟ ΜΑΓΝΗΣΙΟ',
-    formula: [],
+    name: 'magnesiumNitrate',
+    label: 'ΝΙΤΡΙΚΟ ΜΑΓΝΗΣΙΟ',
+    formula: []
   },
   {
     id: 12,
-    name: 'ΙΧΝΟΣΤΟΙΧΕΙΑ',
-    formula: [],
+    name: 'nutrients',
+    label: 'ΙΧΝΟΣΤΟΙΧΕΙΑ',
+    formula: []
   },
   {
     id: 13,
-    name: 'ALGIFLOR',
-    formula: [],
+    name: 'algiflor',
+    label: 'ALGIFLOR',
+    formula: []
   },
   {
     id: 14,
-    name: 'BETAFIN',
-    formula: [],
+    name: 'betafin',
+    label: 'BETAFIN',
+    formula: []
   },
   {
     id: 15,
-    name: 'AMINOL',
-    formula: [],
+    name: 'aminol',
+    label: 'AMINOL',
+    formula: []
   },
   {
     id: 16,
-    name: 'ΣΙΔΗΡΟΣ FERRIPLUS',
-    formula: [],
+    name: 'ferriplus',
+    label: 'ΣΙΔΗΡΟΣ FERRIPLUS',
+    formula: []
   },
   {
     id: 17,
-    name: 'ΒΟΡΑΚΑΣ SOLUBOR',
-    formula: [],
+    name: 'solubor',
+    label: 'ΒΟΡΑΚΑΣ SOLUBOR',
+    formula: []
   },
   {
     id: 18,
-    name: 'ΣΙΔΗΡΟΣ Fe',
-    formula: [],
+    name: 'fe',
+    label: 'ΣΙΔΗΡΟΣ Fe',
+    formula: []
   },
   {
     id: 19,
-    name: 'ΨΕΥΔΑΡΓΗΡΟΣ Zn',
-    formula: [],
+    name: 'zn',
+    label: 'ΨΕΥΔΑΡΓΗΡΟΣ Zn',
+    formula: []
   },
   {
     id: 20,
-    name: 'ΜΑΓΓΑΝΙΟ Mn',
-    formula: [],
+    name: 'mn',
+    label: 'ΜΑΓΓΑΝΙΟ Mn',
+    formula: []
   },
   {
     id: 21,
-    name: 'ΧΑΛΚΟΣ Cu',
-    formula: [],
+    name: 'cu',
+    label: 'ΧΑΛΚΟΣ Cu',
+    formula: []
   },
   {
     id: 22,
-    name: 'MgO EDTA',
-    formula: [],
+    name: 'mgo-edta',
+    label: 'MgO EDTA',
+    formula: []
   },
   {
     id: 23,
-    name: 'Ca EDTA',
-    formula: [],
+    name: 'ca-edta',
+    label: 'Ca EDTA',
+    formula: []
   },
   {
     id: 24,
-    name: 'ΜΟΛΥΒΔΑΙΝΙΟ Mo',
-    formula: [],
-  },
+    name: 'mo',
+    label: 'ΜΟΛΥΒΔΑΙΝΙΟ Mo',
+    formula: []
+  }
 ];
