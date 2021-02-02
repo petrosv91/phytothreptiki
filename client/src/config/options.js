7b8dd6f34e574b45ccb7e39825406fe730df0727
CONNECTLINE\vpe <vpe@connect-lie.gr>
2021-02-02 13:25:54 +0200

    options

--- client/src/config/options.js
+++ client/src/config/options.js
@@ -1,7 +1,5 @@
-export const weights = [5, 10, 15, 20, 22.5, 25, 40, 45, 50];
+const weightsArr = [5, 10, 15, 20, 22.5, 25, 40, 45, 50];
+export const weights = weightsArr.map((val, index) => ({ value: index, label: val }));
 
-export const baseElements = [
-  { value: 1, label: 'Αμμωνιακό' },
-  { value: 2, label: 'Ουρικό' },
-  { value: 3, label: 'Νιτρικό' },
-];
+const baseElementsArr = ['Αμμωνιακό', 'Ουρικό', 'Νιτρικό'];
+export const baseElements = baseElementsArr.map((val, index) => ({ value: index, label: val }));
