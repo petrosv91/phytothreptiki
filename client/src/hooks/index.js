cedcaef81fc400f6d3485ae5ae7be76d9ac29aac
CONNECTLINE\vpe <vpe@connect-lie.gr>
2021-03-15 21:11:09 +0200

    remove unused files

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,7 +1,6 @@
 import useFiltersData from './useFiltersData';
 import useReactFormSchema from './useFormSchema';
 import useLocalStorage from './useLocalStorage';
-import useNavbarTitle from './useNavbarTitle';
 import usePagination from './usePagination';
 import useReactQueryConfig from './useQueryConfig';
 import useWindowSize from './useWindowSize';
@@ -9,7 +8,6 @@ import useWindowSize from './useWindowSize';
 export {
   usePagination,
   useFiltersData,
-  useNavbarTitle,
   useLocalStorage,
   useWindowSize,
   useReactQueryConfig,
