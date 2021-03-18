54e9b9c9be9f387d5c534fd1b1f52e4ae438f178
CONNECTLINE\vpe <vpe@connect-lie.gr>
2021-03-18 12:58:38 +0200

    remove unused

--- client/src/components/print/componentToPrint.js
+++ client/src/components/print/componentToPrint.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import { Box, ChakraProvider, Flex, Image } from '@chakra-ui/react';
+import { Box, Flex, Image } from '@chakra-ui/react';
 import { useFormContext } from 'react-hook-form';
 
 import Logo from '../../assets/logo.png';
-import { lightTheme } from '../../config';
 import { useMainMachine } from '../../context/mainMachineProvider';
 import { FormInput } from '../../lib/ui';
 import ElementStore from '../element/elementStore';
